ab6edc9308c7285ff45b2738aaa328612e422c90
Alvian <alvian.ardhiansyah@gmail.com>
2020-08-17 23:14:13 +0700

    add another controller

--- routes/admin.php
+++ routes/admin.php
@@ -7,6 +7,11 @@ Route::group([
     'namespace' => 'Admin',
     'middleware' => 'auth'
 ], function () {
+
     Route::resource('dashboard', 'DashboardController');
     Route::resource('portofolio', 'PortofolioController');
+    Route::resource('team', 'TeamController');
+    Route::resource('about-us', 'AboutController');
+    Route::resource('service', 'ServiceController');
+
 });
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/coba', function(){
-    return view("auth.login2");
-});
 
 require "frontend.php";
 require "admin.php";
